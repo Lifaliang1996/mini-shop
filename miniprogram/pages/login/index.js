3bf34347da8ffc6832922805e30439f4243ea601
LI_FA_LIANG <274750134@qq.com>
2020-12-24 22:35:41 +0800

    登录页完善校验

--- miniprogram/pages/login/index.js
+++ miniprogram/pages/login/index.js
@@ -1,7 +1,12 @@
 import User from '../../store/user/index'
 
 Page({
-  data: {},
+  data: {
+    phone: '',
+    password: '',
+    disabled: true,
+    isValid: true
+  },
 
   loginByWx (e) {
     const userInfo = e.detail.userInfo
@@ -9,5 +14,24 @@ Page({
       User.userInfo.saveUserInfo(userInfo)
       wx.navigateBack()
     }
+  },
+
+  checkPhone () {
+    console.log(1)
+    const isValid = /^1\d{10}/.test(this.data.phone)
+    this.setData({
+      isValid
+    })
+  },
+
+  loginByPhone () {
+    wx.showLoading()
+    setTimeout(() => {
+      wx.hideLoading()
+      wx.showToast({
+        title: '登录失败，账号或密码错误',
+        icon: 'none'
+    })
+    }, 1500)
   }
 })
